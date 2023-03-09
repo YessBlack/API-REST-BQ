2c72f15a068a909f5444a1d87e197f5faad37483
Angelica Garcia <70681219+YessBlack@users.noreply.github.com>
2023-03-09 16:39:03 -0500

    Update server.js

--- api/server.js
+++ api/server.js
@@ -7,13 +7,23 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
+  '/api/*': '/$1',
+  '/blog/:resource/:id/show': '/:resource/:id'
 }))
 server.use(router)
 server.listen(3000, () => {
-    console.log('JSON Server is running')
+  console.log('JSON Server is running')
+})
+
+const jsonServerAuth = require('json-server-auth')
+const serverAuth = jsonServerAuth.create()
+const routerAuth = jsonServerAuth.router('auth.json')
+serverAuth.use(middlewares)
+serverAuth.use(routerAuth)
+
+serverAuth.listen(3001, () => {
+  console.log('JSON Server Auth is running')
 })
 
 // Export the Server API
-module.exports = server
+module.exports = { server, serverAuth }
